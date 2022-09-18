c16746fb194b0a8277a5e3c34bc2db866cc7a503
Bartosz Panic <bartosz.panic@gmail.com>
2022-09-18 19:25:34 +0200

    Added base entities, Ingredient, recipe and user

---
+++ Recipe.NET.Domain/Entity/Ingredient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.NET.Domain.Entity
+{
+    public class Ingredient
+    {
+        public int IngredientId { get; set; }
+        public Guid IngredientsGuid { get; set; }
+        public string IngredientName { get; set; } = string.Empty;
+    }
+}
